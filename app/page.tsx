35c22f8f1191be85a2f3f4aec61aa8cb2946e27a
2003Talha <talhatariq2003@gmail.com>
2026-02-28 02:12:22 +0500

    Dashboard Integration

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '50px', textAlign: 'center', fontFamily: 'Times New Roman', color: '#dc143c' }}>
+      <h1 style={{ fontSize: '3rem' }}>Dashboard Integration</h1>
+    </main>
+  );
+}
